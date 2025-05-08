29d543bceb06493b282393cd29dad51ad56f378b
Sir-Narly <oomkbhakta@gmail.com>
2025-05-07 19:09:02 -0500

    CalculatorView
    Basic set up of the View class for the buttons and the text views.

--- app/src/main/java/com/example/calculatoralr239/view/CalculatorView.java
+++ app/src/main/java/com/example/calculatoralr239/view/CalculatorView.java
@@ -1,4 +1,49 @@
 package com.example.calculatoralr239.view;
 
+import android.widget.Button;
+import android.widget.TextView;
+
 public class CalculatorView {
+
+    private Button[][] numberPad;
+    private Button[] symbols;
+    private TextView stackNums;
+    private TextView current;
+
+
+    public CalculatorView(Button[][] numberPad, Button[] symbols, TextView stackNums, TextView current){
+
+        this.numberPad = numberPad;
+        this.symbols = symbols;
+        this.stackNums = stackNums;
+        this.current = current;
+
+        int count = 0;
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++) {
+
+                count++;
+                this.numberPad[i][j].setText(count);
+
+            }
+        }
+
+        this.numberPad[3][0].setText("+/-");
+        this.numberPad[3][1].setText("0");
+        this.numberPad[3][2].setText("Enter");
+
+        this.symbols[0].setText("Clear");
+        this.symbols[1].setText("+");
+        this.symbols[2].setText("-");
+        this.symbols[3].setText("X");
+        this.symbols[4].setText("/");
+
+    }
+
+    public void clear(){
+
+    }
+
 }
+
+
